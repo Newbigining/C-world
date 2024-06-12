d978f17dd043b3e47f770623acbc84b208356a10
Abhishek Yadav <abhishekyadav44121@gmail.com>
2024-06-12 12:25:59 +0530

    wright about code

---
+++ Evenoddnumber.c
@@ -0,0 +1,13 @@
+//( This code is to difinr Even and odd numbers )
+#include <Stdio.h>
+int main(){
+  int x;
+printf("Enter value");
+scanf("%d",&x);
+IF (x%2==0)
+{
+printf("Even Number");
+}
+else Printf("Odd Number");
+return o;
+}
